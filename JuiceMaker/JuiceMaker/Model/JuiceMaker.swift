ca258ecbe41ac883238f0055784e72525e83072b
seohyeon2 <seo_hyeon418@naver.com>
2022-04-28 19:53:24 +0900

    test: test파일 생성

--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -2,6 +2,8 @@
 //  JuiceMaker - JuiceMaker.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // 
 
 import Foundation
@@ -9,4 +11,24 @@ import Foundation
 // 쥬스 메이커 타입
 struct JuiceMaker {
     
+=======
+//
+struct JuiceMaker {
+=======
+//
+struct JuiceMaker {
+>>>>>>> Stashed changes
+    private let store = FruitStore()
+    
+    func make(juice: Menu) throws {
+        let isPossible = try store.hasEnoughInventory(of: juice.recipe, in: store.fruitsInventory)
+        
+        if isPossible {
+            try store.reduceInventory(of: juice.recipe)
+            print("\(juice)쥬스 나왔습니다! 맛있게 드세요!")
+        } else {
+            throw AppError.lackOfInventory
+        }
+    }
+>>>>>>> Stashed changes
 }
