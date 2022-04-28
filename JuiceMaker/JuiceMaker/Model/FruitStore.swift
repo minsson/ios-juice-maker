dfbfae85e61440243a46255ed1cabe88c4d89715
minsson <mskaang@gmail.com>
2022-04-28 20:39:28 +0900

    refactor: 리뷰어 피드백 임시 반영 (수정 예정)

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -6,42 +6,43 @@
 
 class FruitStore {
     enum Fruit: CaseIterable {
-        case strawberry
-        case banana
-        case kiwi
-        case pineapple
-        case mango
-    }
-    
-    typealias FruitsInventory = [Fruit: Int]
-    var fruitsInventory: FruitsInventory = [:]
-    
-    init() {
-        let initialInventory = 10
-        Fruit.allCases.forEach { eachFruit in fruitsInventory[eachFruit] = initialInventory }
-    }
-    
-    func reduceInventory(of ingredient: FruitsInventory) throws {
-        for fruit in ingredient.keys {
-            let amountOfIngredient = ingredient[fruit] ?? 0
-            guard var inventory = fruitsInventory[fruit] else { throw AppError.invalidInputOfFruit }
-            
-            print("\(fruit) 사용전 재고: \(inventory)")
-            inventory -= amountOfIngredient
-            print("\(fruit) 사용후 재고: \(inventory)")
-        }
-    }
-    
-    func supplyInventory(of ingredient: FruitsInventory) throws {
-        for fruit in ingredient.keys {
-            let amountOfSupply = ingredient[fruit] ?? 0
-            guard var inventory = fruitsInventory[fruit] else { throw AppError.invalidInputOfFruit }
-            
-            print("\(fruit) 입고전 재고: \(inventory)")
-            inventory += amountOfSupply
-            print("\(fruit) 입고후 재고: \(inventory)")
-        }
-    }
+         case strawberry
+         case banana
+         case kiwi
+         case pineapple
+         case mango
+     }
+
+     typealias FruitsInventory = [Fruit: Int]
+     var fruitsInventory: FruitsInventory = [:]
+
+     init(initialInventory:Int = 10) {
+         Fruit.allCases.forEach { fruitsInventory[$0] = initialInventory }
+     }
+
+     func reduceInventory(of ingredient: FruitsInventory) throws {
+         for fruit in ingredient.keys {
+             let amountOfIngredient = ingredient[fruit] ?? 0
+             guard var inventory = fruitsInventory[fruit] else { throw AppError.invalidInputOfFruit }
+
+             print("\(fruit) 사용전 재고: \(inventory)")
+             inventory -= amountOfIngredient
+             fruitsInventory[fruit] = inventory
+             print("\(fruit) 사용후 재고: \(inventory)")
+         }
+     }
+
+     func supplyInventory(of ingredient: FruitsInventory) throws {
+         for fruit in ingredient.keys {
+             let amountOfSupply = ingredient[fruit] ?? 0
+             guard var inventory = fruitsInventory[fruit] else { throw AppError.invalidInputOfFruit }
+
+             print("\(fruit) 입고전 재고: \(inventory)")
+             inventory += amountOfSupply
+             fruitsInventory[fruit] = inventory
+             print("\(fruit) 입고후 재고: \(inventory)")
+         }
+     }
     
     func hasEnoughInventory(of ingredient: FruitsInventory, in inventory: FruitsInventory) throws -> Bool {
         for fruit in ingredient.keys {
